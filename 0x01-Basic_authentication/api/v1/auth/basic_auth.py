3c6a18f108a4120b5ae60169ef0cf73f04bc9d1a
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-09 00:20:04 -0700

    update basic_auth class to return value of auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """Basic class inherit from the auth"""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic auth inherit from Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """return none in some casses or base64"""
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header and type(authorization_header) == str:
+            list = authorization_header.split(' ')
+            if list[0] != 'Basic':
+                return None
+            else:
+                return list[1]
