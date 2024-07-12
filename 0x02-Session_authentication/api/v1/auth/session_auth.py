d137b82b34cf816efae8be8c8ea088e289ab4398
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-11 19:25:39 -0700

    check retrive user_id from the dict

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id.update({f"{session_id}": f"{user_id}"})
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """retrive the value of the seession_id which is the user_id"""
+        if not session_id and not isinstance(session_id, str):
+            return None
+        user_id = self.user_id_by_session_id.get(f'{session_id}')
+        return user_id
