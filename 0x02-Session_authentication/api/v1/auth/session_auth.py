ae30a6e242477f1dd0170c1bae93b366b82cc08c
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-11 19:19:09 -0700

    check session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """session class inherit from the auth"""
 from .auth import Auth
-import base64
-from typing import TypeVar
-from models.user import User
-
+import uuid
 
 class SessionAuth(Auth):
     """Basic auth inherit from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create session id and assign it to the dic"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id.update({f"{session_id}": f"{user_id}"})
+        return session_id
+
