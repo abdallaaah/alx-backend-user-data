324cd2d90d1c4534d3e312b676097dd6aef0de94
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-12 22:54:14 -0700

    update complete auth for website love cookies

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,9 +6,9 @@ from flask import request, jsonify, make_response
 import os
 
 
-@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def handel_routes_for_auth():
-    """just for now return flask request"""
+    """just for now return flask requesttt"""
     if request.method == 'POST':
         mail = request.form.get('email')
         if not mail:
@@ -18,15 +18,16 @@ def handel_routes_for_auth():
             return jsonify({"error": "password missing"}), 400
 
         email = {'email': mail}
-        user = User.search(email)
-        if not user:
+        users = User.search(email)
+        if not users or users == []:
             return jsonify({"error": "no user found for this email"})
-        if user[0].is_valid_password(passwrod):
-            from api.v1.app import auth
-            session_id = auth.create_session(user[0].id)
-            response = make_response(user[0].to_json())
-            session_name = os.getenv('SESSION_NAME')
-            response.set_cookie(session_name, session_id)
-            return response
-        else:
-            return jsonify({"error": "wrong password"}), 401
+        for user in users:
+            if user.is_valid_password(passwrod):
+                from api.v1.app import auth
+                session_id = auth.create_session(user.id)
+                response = make_response(user.to_json())
+                session_name = os.getenv('SESSION_NAME')
+                response.set_cookie(session_name, session_id)
+                return response
+            else:
+                return jsonify({"error": "wrong password"}), 401
