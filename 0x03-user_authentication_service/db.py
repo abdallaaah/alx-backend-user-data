c4619d44afcf425e30c4b12dcbe9dc7036443da9
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-16 17:11:38 -0700

    check add user function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """add user to users table"""
         new_user = User(email=email, hashed_password=hashed_password)
         session = self._session
