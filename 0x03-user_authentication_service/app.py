bc1a88d76824c4afca93dd135363481c82c19aa5
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-18 12:39:58 -0700

    check task login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def users():
             return jsonify({"message": "email already registered"}), 400
 
 
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
 def login() -> Response:
     """Log in a user and create a session """
     email = request.form.get("email")
