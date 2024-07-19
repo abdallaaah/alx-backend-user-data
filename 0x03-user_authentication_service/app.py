faf67b44b97cda80cdcbaafb77a25db54e346661
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-18 19:43:10 -0700

    check profile implelemnt

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,7 +72,7 @@ def logout() -> str:
 
 
 @app.route("/profile", methods=['GET'], strict_slashes=False)
-def profile():
+def profile() -> str:
     """check profile"""
     session_id = request.cookies.get("session_id")
     if not session_id:
