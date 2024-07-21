c21e188efb489920b8f84ef50d28a86fc738ec28
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-21 04:21:32 -0700

    check task regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """ 0. Regex-ing """
-import logging
 import re
 from typing import List
 
-logger = logging.getLogger(__name__)
 
-
-def filter_datum(fields: [str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: [str], redaction: str,
+                 message: str, separator: str) -> str:
     """that returns the log message obfuscated: """
     for field in fields:
         message = re.sub(
