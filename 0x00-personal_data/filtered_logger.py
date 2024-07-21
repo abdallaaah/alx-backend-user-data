d118be461d5fbc4fbcd2c1bec04e1d7ffb7d469a
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-21 04:47:23 -0700

    check task regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 
+import logging
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -12,3 +14,21 @@ def filter_datum(fields: List[str], redaction: str,
             rf'{re.escape(field)}=.*?(?={separator}|$)',
             f'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format logger"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
