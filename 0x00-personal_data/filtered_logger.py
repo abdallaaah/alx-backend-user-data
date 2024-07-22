bc68300aa82afb0896b117308104ebd48b02193f
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-21 19:16:19 -0700

    check get logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,19 @@ from typing import List
 
 import logging
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+def get_logger() -> logging.Logger:
+    """get logger function"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
