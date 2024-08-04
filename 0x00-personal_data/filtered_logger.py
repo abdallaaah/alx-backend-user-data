f26891c2bf1a543ca8d1e5daab71c3df531ed2ad
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-08-04 14:58:11 -0700

    check database connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ 0. Regex-ing """
+import os
 import re
 from typing import List
-
 import logging
+import mysql.connector
+from mysql.connector import Error
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -17,6 +19,19 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
+
+
+def get_db():
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    data_base_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    try:
+        connection = mysql.connector.connect(host=host, database=data_base_name, username=username, password=password)
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print('Error while connecting database', e)
 
 
 def filter_datum(fields: List[str], redaction: str,
